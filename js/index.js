8e13cc97929f4e8c135a7369e4db1b88b7b3e864
conductiveio <grodin.robby@gmail.com>
2015-09-03 15:05:50 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$('document').ready(function() {
+	var options = { videoId: 'p0enuG9uJYY'
+					, mute: false
+					, volumeUpClass: 'tubular-volume-up'
+					, volumeDownClass: 'tubular-volume-down'
+					, increaseVolumeBy: 10
+		};
+	$('#wrapper').tubular(options);
+});
